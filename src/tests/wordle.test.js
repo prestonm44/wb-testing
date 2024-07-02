e8e5eeb81a88a69cbce8f8b0d9aa8a906f668d8a
prestonm44 <preston.mitchell4444@gmail.com>
2024-07-02 17:24:16 -0600

    finished

--- src/tests/wordle.test.js
+++ src/tests/wordle.test.js
@@ -1,9 +1,10 @@
 import { jest } from '@jest/globals';
 
+const mockIsWord = jest.fn(() => true);
 jest.unstable_mockModule('../words.js', () => {
     return {
       getWord: jest.fn(() => 'APPLE'),
-      isWord: jest.fn(() => true),
+      isWord: mockIsWord
     };
   });
 
@@ -47,13 +48,13 @@ describe('constructing a new Wordle game', () => {
     test('sets the status of a correct letter to CORRECT', () => {
         const wordle = new Wordle();
         const guess = wordle.buildGuessFromWord('A____');
-        expect(guess[0].status).toBE('CORRECT')
+        expect(guess[0].status).toBe('CORRECT')
     })
 
     test('sets the status of a present letter to PRESENT', () => {
         const wordle = new Wordle();
         const guess = wordle.buildGuessFromWord('E____');
-        expect(guess[0].status).toBE('PRESENT')
+        expect(guess[0].status).toBe('PRESENT')
     })
 
     test('sets the status of an absent letter to ABSENT', () => {
@@ -62,3 +63,73 @@ describe('constructing a new Wordle game', () => {
         expect(guess[0].status).toBe('ABSENT')
     })
 })
+
+describe('making a guess', () => {
+    test('throws an error if no more guesses are allowed', () => {
+      const wordle = new Wordle(1);
+      wordle.appendGuess('GUESS');
+      expect(() => wordle.appendGuess('GUESS')).toThrow();
+    });
+  
+    test('throws an error if the guess is not of length 5', () => {
+      const wordle = new Wordle();
+      expect(() => wordle.appendGuess('LONG GUESS')).toThrow();
+    });
+  
+    test('throws an error if the guess is not a word', () => {
+      const wordle = new Wordle();
+      mockIsWord.mockReturnValueOnce(false);
+      expect(() => wordle.appendGuess('GUESS')).toThrow();
+    });
+  
+    test('appends the result of buildGuessFromWord to the guesses array', () => {
+      const wordle = new Wordle();
+      wordle.appendGuess('GUESS');
+      expect(wordle.guesses[0]).toEqual(wordle.buildGuessFromWord('GUESS'));
+    });
+  
+    test('increments the current guess', () => {
+      const wordle = new Wordle();
+      wordle.appendGuess('GUESS');
+      expect(wordle.currGuess).toBe(1);
+    });
+  });
+
+describe('testing to see if Wordle is solved', () => {
+    test('lastest guess is correct word', () => {
+      const wordle = new Wordle();
+      wordle.appendGuess('APPLE');
+      expect(wordle.isSolved()).toBe(true);
+    })
+
+    test('returns false if the latest guess is not the correct word', () => {
+        const wordle = new Wordle();
+        wordle.appendGuess('GUESS');
+        expect(wordle.isSolved()).toBe(false);
+      });
+})
+
+describe('checking if the game should end', () => {
+    test('returns true if the latest guess is the correct word', () => {
+      const wordle = new Wordle();
+      wordle.appendGuess('APPLE');
+      expect(wordle.shouldEndGame()).toBe(true);
+    });
+  
+    test('returns true if there are no more guesses left', () => {
+      const wordle = new Wordle(1);
+      wordle.appendGuess('GUESS');
+      expect(wordle.shouldEndGame()).toBe(true);
+    });
+  
+    test('returns false if no guess has been made', () => {
+      const wordle = new Wordle();
+      expect(wordle.shouldEndGame()).toBe(false);
+    });
+  
+    test('returns false if there are guesses left and the word has not been guessed', () => {
+      const wordle = new Wordle();
+      wordle.appendGuess('GUESS');
+      expect(wordle.shouldEndGame()).toBe(false);
+    });
+  });
